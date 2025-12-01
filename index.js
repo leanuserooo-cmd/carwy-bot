71575be602c989dfad6e8d0df057fcd2bfe3a63f
leanuserooo-cmd <leanuserooo@gmail.com>
2025-12-01 13:52:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const app = express();
 app.use(bodyParser.json());
 
-// ULTRAMSG datos tuyos
+// ULTRAMSG datos
 const INSTANCE_ID = "instance153389";
 const TOKEN = "su9tSamyvokrgrubf";
 
@@ -13,16 +13,28 @@ const TOKEN = "su9tSamyvokrgrubf";
 const GRUPO = "Kk7ec2otdba3kj5TZ2flas@g.us";
 
 app.post("/webhook", async (req, res) => {
-    const text = req.body?.body?.message?.text || "";
-    if (!text) return res.sendStatus(200);
+    try {
 
-    await axios.post(https://api.ultramsg.com/${INSTANCE_ID}/messages/chat, {
-        token: TOKEN,
-        to: GRUPO,
-        body: text
-    });
+        const text = req.body?.body?.message?.text || "";
+        if (!text) return res.sendStatus(200);
 
-    res.sendStatus(200);
+        console.log("Cliente escribió:", text);
+
+        await axios.post(https://api.ultramsg.com/${INSTANCE_ID}/messages/group, {
+            token: TOKEN,
+            to: GRUPO,
+            body: text
+        });
+
+        console.log("Mensaje enviado al grupo!");
+        res.sendStatus(200);
+
+    } catch (error) {
+        console.log("ERROR reenviando:", error.message);
+        res.sendStatus(500);
+    }
 });
 
-app.listen(10000, () => console.log("BOT CarWy ON"));
+app.listen(3000, () => {
+    console.log("BOT CarWy listo en puerto 3000");
+});
